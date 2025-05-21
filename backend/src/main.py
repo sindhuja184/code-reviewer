12395d9c0e6215ade5ad04af200df5bade0276da
sindhuja184 <dapsindhuja@gmail.com>
2025-05-21 19:14:59 +0530

    Added middleware

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
 from auth.routes import auth_router
 from code_rev.routes import code_router
+from middleware import register_middleware 
+
 version = 'v1'
 
-app = FastAPI()
+app = FastAPI(
+    title = "Code Reviewer",
+    description = "A REST API for code review",
+    version=version
+)
 
+
+register_middleware(app=app)
 
 app.include_router(auth_router, prefix = f'/api/{version}/auth')
 app.include_router(code_router, prefix = f'/api/{version}/codereview')
